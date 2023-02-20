42bfff199bb43e1455813fe13ff2d5efd3a1c8a8
Michiel Min <michiel.min@gmail.com>
2023-02-20 17:04:51 +0100

    Error fixed in setting abundances for the clouds

--- SetupCloud.f
+++ SetupCloud.f
@@ -72,6 +72,9 @@ c 90% MgSiO3
 			Cloud(ii)%rv(1:nr)=Cloud(ii)%rnuc+(Cloud(ii)%reff-Cloud(ii)%rnuc)*(P(1:nr)/Cloud(ii)%Pref)**Cloud(ii)%rpow
 			Cloud(ii)%sigma(1:nr)=Cloud(ii)%veff
 			Cloud(ii)%onepart=(Cloud(ii)%rpow.eq.0d0)
+			do i=1,nr
+				Cloud(i)%frac(i,1:40)=Cloud(i)%abun(1:40)
+			enddo
 			call SetupPartCloud(ii)
 			do i=1,nr
 				Cloud(ii)%Kref=Cloud(ii)%Kext(i,nlam+1)
@@ -88,6 +91,9 @@ c 90% MgSiO3
 			Cloud(ii)%rv(1:nr)=Cloud(ii)%rnuc+(Cloud(ii)%reff-Cloud(ii)%rnuc)*(P(1:nr)/Cloud(ii)%Pref)**Cloud(ii)%rpow
 			Cloud(ii)%sigma(1:nr)=Cloud(ii)%veff
 			Cloud(ii)%onepart=(Cloud(ii)%rpow.eq.0d0)
+			do i=1,nr
+				Cloud(i)%frac(i,1:40)=Cloud(i)%abun(1:40)
+			enddo
 			call SetupPartCloud(ii)
 			do i=1,nr
 				Cloud(ii)%Kref=Cloud(ii)%Kext(i,nlam+1)
@@ -104,6 +110,9 @@ c 90% MgSiO3
 			Cloud(ii)%rv(1:nr)=Cloud(ii)%rnuc+(Cloud(ii)%reff-Cloud(ii)%rnuc)*(P(1:nr)/Cloud(ii)%Pref)**Cloud(ii)%rpow
 			Cloud(ii)%sigma(1:nr)=Cloud(ii)%veff
 			Cloud(ii)%onepart=(Cloud(ii)%rpow.eq.0d0)
+			do i=1,nr
+				Cloud(i)%frac(i,1:40)=Cloud(i)%abun(1:40)
+			enddo
 			call SetupPartCloud(ii)
 			do i=1,nr
 				Cloud(ii)%Kref=Cloud(ii)%Kext(i,nlam+1)
@@ -117,6 +126,9 @@ c 90% MgSiO3
 			Cloud(ii)%rv(1:nr)=Cloud(ii)%rnuc+(Cloud(ii)%reff-Cloud(ii)%rnuc)*(P(1:nr)/Cloud(ii)%Pref)**Cloud(ii)%rpow
 			Cloud(ii)%sigma(1:nr)=Cloud(ii)%veff
 			Cloud(ii)%onepart=(Cloud(ii)%rpow.eq.0d0)
+			do i=1,nr
+				Cloud(i)%frac(i,1:40)=Cloud(i)%abun(1:40)
+			enddo
 			call SetupPartCloud(ii)
 		case default
 			call output("Cloud type unknown: " // trim(Cloud(ii)%type))
